ff3c19674732993aa81396e19938877dd2191e82
Fatmagül Gök <afsgok28@gmail.com>
2025-08-24 18:06:42 +0100

    create new file for demo of alpha-spending function comparison

---
+++ alpha-spending.R
@@ -0,0 +1,16 @@
+library(ldbounds)
+
+n = c(1, 31,24,46,13)  # set the first sample size to 1 so that plot starts close to origin
+
+obf_bounds <- ldBounds(t = cumsum(n)/sum(n), alpha = 0.05/4 , sides = 1, iuse = 1)
+p_bounds <- ldBounds(t = cumsum(n)/sum(n), alpha = 0.05/4 , sides = 1, iuse = 2)
+
+plot(obf_bounds$time, obf_bounds$exit.pr, type = 'o', lty = 3, pch = 16, 
+     ylim = c(0, bounds$alpha), 
+     ylab = 'Type I error probability', xlab = 'Information fraction')
+lines(p_bounds$time, p_bounds$exit.pr, type = 'o', lty = 1, pch = 16)
+abline(h = obf_bounds$alpha, lty = 2, col = 'darkblue')
+
+legend('bottomright', legend = c("Overall alpha","Pocock", "O'Brien-Fleming"), 
+       lty = c(2,1,3), pch = c(NA, 16, 16), col = c('darkblue', 'black', 'black'),
+       bty = 'n')
