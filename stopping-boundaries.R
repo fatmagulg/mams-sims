d52dee95003eb7c3dd62909ff0220f8cb57ce609
Fatmagül Gök <afsgok28@gmail.com>
2025-08-25 12:54:09 +0100

    improve labels on plot

--- stopping-boundaries.R
+++ stopping-boundaries.R
@@ -9,7 +9,10 @@ looks <- seq(length(p_bounds$upper.bounds))
 
 plot(looks, p_bounds$upper.bounds, type = 'o', lty = 3, pch = 16, 
      ylim = c(-6,6), xlim = c(1, 5.5), 
-     ylab = )
+     ylab = "Normal test statistic", xlab = "Interim analysis")
+
+lines(c(5,5), c(-2.41311,2.41311), col = 'darkgray')
+
 lines(looks, p_bounds$lower.bounds, type = 'o', lty = 3, pch = 16)
 
 lines(looks, obf_bounds$upper, type = 'o', lty = 1, pch = 17)
@@ -18,13 +21,14 @@ lines(looks, obf_bounds$lower.bounds, type = 'o', lty = 1, pch = 17)
 lines(looks, c(rep(3, 4), 1.96), type = 'o', lty = 2, pch = 18)
 lines(looks, c(rep(-3, 4), -1.96), type = 'o', lty = 2, pch = 18)
 
-lines(c(5,5), c(-2.41311,2.41311), col = 'darkgray')
+
 
 legend('bottomright', legend = c("Haybittle-Peto", "Pocock", "O'Brien-Fleming"), 
        lty = c(2,3,1), pch = c(18, 16,17), bty = 'n')
 
-text(x = 3, y = -5, "Reject null")
-text(x = 3, y = 5, "Reject null")
+text(x = 3, y = -5, expression(Reject ~ H[0]))
+text(x = 3, y = 5, expression(Reject ~ H[0]))
 text(x = 3, y = 0, "Continue")
 text(x = 5.3, y = 0.25, "Accept")
-text(x = 5.3, y = -0.25, "null")
+text(x = 5.3, y = -0.25, expression(H[0]))
+
